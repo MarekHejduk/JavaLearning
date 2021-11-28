6a5c3a6ce2ab64825bd17c10c5da3428c3b63fac
Marek Hejduk (ext) <mhejduk-ext@ad.xitee.com>
2021-11-28 18:33:33 +0100

    pridej tridu BarakNaOdstrel

---
+++ 02_Objekty/BarakNaOdstrel.java
@@ -0,0 +1,13 @@
+public class BarakNaOdstrel{
+    
+    public BarakNaOdstrel(){
+        new Trojuhelnik(0, 0, 150, 75, Barva.CERVENA);
+        new Obdelnik(0, 75, 150, 75, Barva.ZELENA);
+        new Obdelnik(10, 95, 22, 22, Barva.ZLUTA);
+        new Obdelnik(42, 95, 22, 22, Barva.ZLUTA);
+        new Obdelnik(84, 100, 30, 50, Barva.HNEDA);
+        
+    }
+    
+    
+}
---
+++ 02_Objekty/MojeMetody.java
@@ -0,0 +1,29 @@
+public class MojeMetody{
+    private int vek;
+    private Obdelnik obdelnik;
+
+    /**
+     * 1) konstruktor = speciální pøípad metody, díky kterému vytvoøíme NOVÝ objekt
+     * 2) konstruktor nemá návratový typ
+     * 3) konstruktor musí mít stejný název jako tøída
+     */
+    public MojeMetody(){
+        Obdelnik obdelnikObjekt = new Obdelnik();
+        String obdelnikString = "obdelnik";
+        obdelnikObjekt.posunVlevo();
+    }
+    
+    /**
+     * vzor obyèejné metody
+     */
+    public void testovaciMetoda(){
+        vek = 3;
+        obdelnik = new Obdelnik();
+        
+        System.out.println(vek);
+        System.out.println(obdelnik);
+
+        
+    }
+    
+}
