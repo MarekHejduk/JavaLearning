49755ca1daef08c14e289cbd62f3fc8937d4d79e
Marek Hejduk (ext) <mhejduk-ext@ad.xitee.com>
2021-12-10 19:23:49 +0100

    pridej kus panelaku

--- 02_Objekty/Panelak.java
+++ 02_Objekty/Panelak.java
@@ -2,8 +2,8 @@ public class Panelak{
     
     private Obdelnik zdi;
     private Trojuhelnik strecha;
-    private Obdelnik okno;
     private Obdelnik dvere;
+    private Obdelnik okno;
     private int sirka;
     private int vyska;
 
@@ -14,7 +14,8 @@ public class Panelak{
     
         zdi = new Obdelnik(50, 50, 300, 700, Barva.BILA);
         strecha = new Trojuhelnik(50, 0, 300, 50, Barva.CERVENA);
-        
+        dvere = new Obdelnik(184, 690, 25, 60, Barva.CERNA);
+
         
         
         
--- 02_Objekty/Test.java
+++ 02_Objekty/Test.java
@@ -3,11 +3,11 @@ public class Test{
     public Test(){
         Strom strom = new Strom();
         strom.setPozice(55, 75);
-        if (strom.x == 55){
-            System.out.println("Pøesunuto v poøádku.");
-        } else{
-            System.out.println("Chybná pozice.");
-        }
+ //       if (strom.x == 55){
+//            System.out.println("Pøesunuto v poøádku.");
+ //       } else{
+ //           System.out.println("Chybná pozice.");
+  //      }
     }
     
 }
